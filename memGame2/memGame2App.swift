a7ed388039e850437f0a7b0375d41f602017bb3a
Dave Norvall <dave@culvertengineering.com>
2025-11-10 21:39:53 -0800

    Initial Commit

---
+++ memGame2/memGame2App.swift
@@ -0,0 +1,17 @@
+//
+//  memGame2App.swift
+//  memGame2
+//
+//  Created by David Norvall on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct memGame2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ memGame2Tests/memGame2Tests.swift
@@ -0,0 +1,16 @@
+//
+//  memGame2Tests.swift
+//  memGame2Tests
+//
+//  Created by David Norvall on 11/10/25.
+//
+
+import Testing
+
+struct memGame2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
